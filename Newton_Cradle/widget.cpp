3db8237d5b48e202445fca50f968bbe92bc769d9
zhuangzuoyi <ye496430343@163.com>
2018-08-07 09:21:04 +0800

    update newton cradle

--- Newton_Cradle/widget.cpp
+++ Newton_Cradle/widget.cpp
@@ -1,6 +1,7 @@
 #include "widget.h"
 #include "QPainter"
 #include "QtGui"
+#include "QDebug"
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -10,15 +11,218 @@ Widget::Widget(QWidget *parent)
     setPalette(QPalette(Qt::white));
     setAutoFillBackground(true);
     setMinimumSize(400,400);
-  //  Circle_point = new QPoint();
+    canvas_width = 640;
+    canvas_height = 480;
+    resize(640,480);
     radius = 30;
+    ball_num = 5;
+    qDebug()<<"Size is:"<<width()<<","<<height();
+    up_blank = 80;
+    down_blank = 100;
+    moving_ball = 0;
+    newton_cradle_init();
+
+    timer_interval = 50;
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this,SLOT(timeout()));
+    timer->setInterval(timer_interval);
 }
+
+
+void Widget::timeout()
+{
+
+}
+
 
 Widget::~Widget()
 {
 
 }
 
+void Widget::drawFirst_ball(QPainter *p)
+{
+    p->save();
+    QRect circle_rect;
+    QPoint Start_pont,End_point;
+    QPoint lg_start,lg_end;
+    Start_pont.setX(first_ball_center.x() -radius);
+    Start_pont.setY(first_ball_center.y()-radius);
+    End_point.setX(first_ball_center.x() +radius);
+    End_point.setY(first_ball_center.y()+radius);
+
+    lg_start.setX(Start_pont.x()+radius);
+    lg_start.setY(Start_pont.y());
+    lg_end.setX(End_point.x()+radius);
+    lg_end.setY(End_point.y());
+    QLinearGradient lg(lg_start,lg_end);
+    lg.setColorAt(0,Qt::white);
+    lg.setColorAt(1,Qt::black);
+    p->setBrush(lg);
+    circle_rect.setBottomRight(End_point);
+    circle_rect.setTopLeft(Start_pont);
+
+    p->drawLine(first_line_start,first_line_end);
+    p->drawEllipse(circle_rect);
+    p->restore();
+}
+
+void Widget::drawSecond_ball(QPainter *p)
+{
+    p->save();
+    QRect circle_rect;
+    QPoint Start_pont,End_point;
+    QPoint lg_start,lg_end;
+    Start_pont.setX(second_ball_center.x() -radius);
+    Start_pont.setY(second_ball_center.y()-radius);
+    End_point.setX(second_ball_center.x() +radius);
+    End_point.setY(second_ball_center.y()+radius);
+
+    lg_start.setX(Start_pont.x()+radius);
+    lg_start.setY(Start_pont.y());
+    lg_end.setX(End_point.x()+radius);
+    lg_end.setY(End_point.y());
+    QLinearGradient lg(lg_start,lg_end);
+    lg.setColorAt(0,Qt::white);
+    lg.setColorAt(1,Qt::black);
+    p->setBrush(lg);
+    circle_rect.setBottomRight(End_point);
+    circle_rect.setTopLeft(Start_pont);
+
+    p->drawLine(second_line_start,second_line_end);
+    p->drawEllipse(circle_rect);
+    p->restore();
+}
+
+void Widget::drawThird_ball(QPainter *p)
+{
+    p->save();
+    QRect circle_rect;
+    QPoint Start_pont,End_point;
+    QPoint lg_start,lg_end;
+    Start_pont.setX(third_ball_center.x() -radius);
+    Start_pont.setY(third_ball_center.y()-radius);
+    End_point.setX(third_ball_center.x() +radius);
+    End_point.setY(third_ball_center.y()+radius);
+
+    lg_start.setX(Start_pont.x()+radius);
+    lg_start.setY(Start_pont.y());
+    lg_end.setX(End_point.x()+radius);
+    lg_end.setY(End_point.y());
+    QLinearGradient lg(lg_start,lg_end);
+    lg.setColorAt(0,Qt::white);
+    lg.setColorAt(1,Qt::black);
+    p->setBrush(lg);
+    circle_rect.setBottomRight(End_point);
+    circle_rect.setTopLeft(Start_pont);
+
+    p->drawLine(third_line_start,third_line_end);
+    p->drawEllipse(circle_rect);
+    p->restore();
+}
+
+void Widget::drawfourth_ball(QPainter *p)
+{
+    p->save();
+    QRect circle_rect;
+    QPoint Start_pont,End_point;
+    QPoint lg_start,lg_end;
+    Start_pont.setX(forth_ball_center.x() -radius);
+    Start_pont.setY(forth_ball_center.y()-radius);
+    End_point.setX(forth_ball_center.x() +radius);
+    End_point.setY(forth_ball_center.y()+radius);
+
+    lg_start.setX(Start_pont.x()+radius);
+    lg_start.setY(Start_pont.y());
+    lg_end.setX(End_point.x()+radius);
+    lg_end.setY(End_point.y());
+    QLinearGradient lg(lg_start,lg_end);
+    lg.setColorAt(0,Qt::white);
+    lg.setColorAt(1,Qt::black);
+    p->setBrush(lg);
+    circle_rect.setBottomRight(End_point);
+    circle_rect.setTopLeft(Start_pont);
+
+    p->drawLine(forth_line_start,forth_line_end);
+    p->drawEllipse(circle_rect);
+    p->restore();
+}
+
+void Widget::drawfifth_ball(QPainter *p)
+{
+    p->save();
+    QRect circle_rect;
+    QPoint Start_pont,End_point;
+    QPoint lg_start,lg_end;
+    Start_pont.setX(firth_ball_center.x() -radius);
+    Start_pont.setY(firth_ball_center.y()-radius);
+    End_point.setX(firth_ball_center.x() +radius);
+    End_point.setY(firth_ball_center.y()+radius);
+
+    lg_start.setX(Start_pont.x()+radius);
+    lg_start.setY(Start_pont.y());
+    lg_end.setX(End_point.x()+radius);
+    lg_end.setY(End_point.y());
+    QLinearGradient lg(lg_start,lg_end);
+    lg.setColorAt(0,Qt::white);
+    lg.setColorAt(1,Qt::black);
+    p->setBrush(lg);
+    circle_rect.setBottomRight(End_point);
+    circle_rect.setTopLeft(Start_pont);
+
+    p->drawLine(firth_line_start,firth_line_end);
+    p->drawEllipse(circle_rect);
+    p->restore();
+}
+
+
+void Widget::newton_cradle_init(void)
+{
+    //int x_temp,y_temp;
+    int left_start = (width() - radius * 2 * (ball_num-1)) / 2;
+    int botton_start = height() - down_blank - radius;
+
+    line_len = botton_start - up_blank;
+    first_line_start.setX(left_start);
+    first_line_start.setY(up_blank);
+    first_line_end.setX(left_start);
+    first_line_end.setY(botton_start);
+    first_ball_center.setX(left_start);
+    first_ball_center.setY(botton_start);
+    first_ball_original_centor.setX(left_start);
+    first_ball_original_centor.setY(botton_start);
+
+    second_line_start.setX(left_start+60);
+    second_line_start.setY(up_blank);
+    second_line_end.setX(left_start+60);
+    second_line_end.setY(botton_start);
+    second_ball_center.setX(left_start+60);
+    second_ball_center.setY(botton_start);
+
+
+    third_line_start.setX(left_start+120);
+    third_line_start.setY(up_blank);
+    third_line_end.setX(left_start+120);
+    third_line_end.setY(botton_start);
+    third_ball_center.setX(left_start+120);
+    third_ball_center.setY(botton_start);
+
+    forth_line_start.setX(left_start+180);
+    forth_line_start.setY(up_blank);
+    forth_line_end.setX(left_start+180);
+    forth_line_end.setY(botton_start);
+    forth_ball_center.setX(left_start+180);
+    forth_ball_center.setY(botton_start);
+
+    firth_line_start.setX(left_start+240);
+    firth_line_start.setY(up_blank);
+    firth_line_end.setX(left_start+240);
+    firth_line_end.setY(botton_start);
+    firth_ball_center.setX(left_start+240);
+    firth_ball_center.setY(botton_start);
+    firth_ball_original_centor.setX(left_start+240);
+    firth_ball_original_centor.setY(botton_start);
+}
 
 void Widget::paintEvent(QPaintEvent *)
 {
@@ -32,25 +236,91 @@ void Widget::paintEvent(QPaintEvent *)
     p.setBrush(lg);
     p.drawEllipse(rect);
     */
-    drawCircle(&p);
+ //   drawline(&p);
+ //   drawCircle(&p);
+    drawFirst_ball(&p);
+    drawSecond_ball(&p);
+    drawThird_ball(&p);
+    drawfourth_ball(&p);
+    drawfifth_ball(&p);
 }
 
 
 void Widget::mouseMoveEvent(QMouseEvent *e)
 {
     qDebug()<<"The mouse's pos is:"<<e->pos().x()<<","<<e->pos().y();
+    if(e->pos().y() > (height() - down_blank - radius))
+        return;
+    if(moving_ball ==1)
+    {
+        //the moving ball is the first ball
+        if(e->pos().x() < first_ball_original_centor.x())
+        {
+            moving_point_old.setX(e->pos().x());
+            moving_point_old.setY(e->pos().y());
+            first_ball_center.setX(moving_point_old.x());
+            first_ball_center.setY(moving_point_old.y());
+            first_line_end.setX(moving_point_old.x());
+            first_line_end.setY(moving_point_old.y());
+        }
+    }else if(moving_ball == 2)
+    {
+        //the moving ball is the firth ball
+
+        if(e->pos().x() > firth_ball_original_centor.x())
+        {
+            moving_point_old.setX(e->pos().x());
+            moving_point_old.setY(e->pos().y());
+            firth_ball_center.setX(moving_point_old.x());
+            firth_ball_center.setY(moving_point_old.y());
+            firth_line_end.setX(moving_point_old.x());
+            firth_line_end.setY(moving_point_old.y());
+        }
+        qDebug()<<"moving the last ball";
+
+    }
+    qDebug()<<"update";
+    update();
+}
+
+void Widget::mouseReleaseEvent(QMouseEvent *)
+{
+ moving_ball = 0;
 }
 
 
 void Widget::mousePressEvent(QMouseEvent *e)
 {
+    int f_x_abs,f_y_abs;
+    int l_x_abs,l_y_abs;
     qDebug()<<"Mouse press:"<<e->pos().x()<<","<<e->pos().y();
-    Circle_point.setX(e->pos().x());
-    Circle_point.setY(e->pos().y());
-    update();
+    f_x_abs = qAbs(first_ball_center.x() - e->pos().x());
+    f_y_abs = qAbs(first_ball_center.y() - e->pos().y());
 
+    l_x_abs = qAbs(firth_ball_center.x() - e->pos().x());
+    l_y_abs = qAbs(firth_ball_center.y() - e->pos().y());
+
+    /*
+    int moving_ball;
+    QPoint moving_point_old;
+    */
+    if(f_x_abs < radius && f_y_abs < radius )
+    {
+        moving_ball = 1;
+        moving_point_old.setX(first_ball_original_centor.x());
+        moving_point_old.setY(first_ball_original_centor.y());
+    }else if(l_x_abs < radius && l_y_abs <radius)
+    {
+        moving_ball = 2;
+        moving_point_old.setX(firth_ball_original_centor.x());
+        moving_point_old.setY(firth_ball_original_centor.y());
+    }
+
+//    Circle_point.setX(e->pos().x());
+//    Circle_point.setY(e->pos().y());
+//    update();
 }
-
+/*
 void Widget::drawCircle(QPainter *p)
 {
     p->save();
@@ -61,23 +331,7 @@ void Widget::drawCircle(QPainter *p)
     Start_pont.setY(Circle_point.y()-radius);
     End_point.setX(Circle_point.x() +radius);
     End_point.setY(Circle_point.y()+radius);
-    /*
-    if(Circle_point.x() >radius && Circle_point.y() >radius)
-    {
-        Start_pont.setX(Circle_point.x() -radius);
-        Start_pont.setY(Circle_point.y()-radius);
-        End_point.setX(Circle_point.x() +radius);
-        End_point.setY(Circle_point.y()+radius);
-    }
-    else
-    {
 
-     //   Start_pont.setX(0);
-     //   Start_pont.setY(0);
-//        End_point.setX(radius);
-     //   End_point.setY(radius);
-
-    }*/
     lg_start.setX(Start_pont.x()+radius);
     lg_start.setY(Start_pont.y());
     lg_end.setX(End_point.x()+radius);
@@ -91,3 +345,17 @@ void Widget::drawCircle(QPainter *p)
     p->drawEllipse(circle_rect);
     p->restore();
 }
+
+
+void Widget::drawline(QPainter *p)
+{
+    p->save();
+    QPoint line_start,line_end;
+    line_start.setX(Circle_point.x());
+    line_start.setY(80);
+    line_end.setX(Circle_point.x());
+    line_end.setY(Circle_point.y());
+    p->drawLine(line_start,line_end);
+    p->restore();
+}
+*/
--- Newton_Cradle/widget.h
+++ Newton_Cradle/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include "QTimer"
 
 class Widget : public QWidget
 {
@@ -13,10 +14,53 @@ public:
     void paintEvent(QPaintEvent *);
     void mouseMoveEvent(QMouseEvent *e);
     void mousePressEvent(QMouseEvent *e);
-    void drawCircle(QPainter *p);
+    //void drawCircle(QPainter *p);
+    //void drawline(QPainter *p);
+    void newton_cradle_init(void);
+
+    void drawFirst_ball(QPainter *p);
+    void drawSecond_ball(QPainter *p);
+    void drawThird_ball(QPainter *p);
+    void drawfourth_ball(QPainter *p);
+    void drawfifth_ball(QPainter *p);
+    void mouseReleaseEvent(QMouseEvent *);
 private:
      QPoint Circle_point;
+     int canvas_width;
+     int canvas_height;
+     int ball_num;
      int radius;
+     int up_blank;
+     int down_blank;
+     int moving_ball;
+     QPoint moving_point_old;
+     QPoint first_ball_center;
+     QPoint first_line_start;
+     QPoint first_line_end;
+     QPoint first_ball_original_centor;
+
+     QPoint second_ball_center;
+     QPoint second_line_start;
+     QPoint second_line_end;
+
+     QPoint third_ball_center;
+     QPoint third_line_start;
+     QPoint third_line_end;
+
+     QPoint forth_ball_center;
+     QPoint forth_line_start;
+     QPoint forth_line_end;
+
+     QPoint firth_ball_center;
+     QPoint firth_line_start;
+     QPoint firth_line_end;
+     QPoint firth_ball_original_centor;
+
+     int line_len;
+     QTimer *timer;
+     int timer_interval;
+private  slots:
+    void timeout();
 };
 
 #endif // WIDGET_H
